6af1381af50b18b1f83b44701a24b6918203b870
Abel Kalum <chumbabel@gmail.com>
2020-01-23 14:07:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,10 @@
 number = 10
 
 def countdown
-while number > 0
-  puts "#{number} SECOND(S)!"
-  number += 1
+  while number > 0
+  
+   puts "#{number} SECOND(S)!"
+   number += 1
   when number = 0
     puts "HAPPY NEW YEAR!"
 end
